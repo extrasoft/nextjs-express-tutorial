944e6a44b75fce7111f2d62664ab2e71ebdc9db8
Team <innovisor.project@gmail.com>
2019-03-13 15:02:32 +0700

    import css

--- server/components/Layout.js
+++ server/components/Layout.js
@@ -3,6 +3,10 @@ import Header from './Header';
 import Footer from './Footer';
 import Head from 'next/head';
 
+// การ import ชื่อไฟล์ที่เป็น static จะต้องติดตั้ง module next-css หรือ next-sass ก่อน
+// และต้องไป config ไฟล์ที่ next.config.js ด้วย
+import '../static/css/style.css';
+
 class Layout extends Component {
   render() {
     const { children, title = 'Next JS' } = this.props; //Props ของ layout
@@ -15,7 +19,11 @@ class Layout extends Component {
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
           <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"/>
-          <link rel="stylesheet" href="../static/css/style.css"/>
+          {/* สำหรับไม่ได้ใช้การ import */}
+          {/* <link rel="stylesheet" href="../static/css/style.css"/> */}
+
+          {/* ถ้าเราใช้จากการ import มามันจะถูก pack และ bundle ไว้ที่ folder _next เราต้องเติม path ใหม่ */}
+          <link rel="stylesheet" href="/_next/static/style.css"/>
 
           <title>{title}</title>
         </Head>
---
+++ server/next.config.js
@@ -0,0 +1,3 @@
+//กรณีที่จะใช้การ import css จะต้องทำไฟล์ config ด้วย
+const withCSS = require('@zeit/next-css');
+module.exports = withCSS();
