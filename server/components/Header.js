1ac5288f187e9b93680ea70e58c7932e68d029b1
Team <innovisor.project@gmail.com>
2019-03-13 11:01:23 +0700

    using next/head + bootstrap

---
+++ server/components/Header.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+    // console.log('contructor');
+    //console.log(this.state.date);
+  }
+
+  componentDidMount() {
+    // console.log('componentDidMount');
+    this.timerID = setInterval(() => this.tick(), 1000);
+  }
+
+  componentDidUpdate() {
+    // console.log('componentDidUpdate');
+  }
+
+  componentWillUnmount() {
+    // console.log('componentWillUnmount');
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState( {date: new Date()} );
+  }
+
+  render() {
+    const styles = { height: 70 };
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-8 text-left">
+            <h1 className="text-success">
+              <img src='/static/images/logo/logo.png' style={styles} alt="" /> เฮลตี้ คาเฟ่
+            </h1> 
+          </div>
+          <div className="col-md-4 text-right">
+            <h5 className="title text-muted mt-4">
+              {this.state.date.toLocaleTimeString()}
+            </h5>
+            <ul className='list-inline'>
+              <li className='list-inline-item'>Home</li>
+              <li className='list-inline-item'>|</li>
+              <li className='list-inline-item'>About</li>
+            </ul>
+          </div>
+        </div>
+        <hr />
+      </div>
+    );
+  }
+}
+
+export default Header;
